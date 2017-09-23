5a44b9e107300a85ef053aaed1f5f5ff5a303cca
Uoc Nguyen <vanuoc.haui@gmail.com>
2017-09-24 00:24:31 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,71 @@
+$('.slider-expert').slick({
+  dots: false,
+  arrows:false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.list-customers').slick({
+  dots: false,
+  arrows:false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 6,
+  slidesToScroll: 6,
+  responsive: [
+    {
+      breakpoint: 991,
+      settings: {
+        slidesToShow: 5,
+        slidesToScroll: 5
+      }
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 4,
+        slidesToScroll: 4
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+$('.nav-slider .nav-prev').click(function(){
+  $('.slider-expert').slick('slickPrev');
+});
 
+$('.nav-slider .nav-next').click(function(){
+  $('.slider-expert').slick('slickNext');
+});
 var LP=new function __LP(){
     this.init=function(){
         $(window).scroll(function(){
